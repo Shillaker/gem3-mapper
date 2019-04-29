622ec9841f7668454d468173ff150cdcf5348441
Simon Shillaker <mail@simonshillaker.com>
2019-04-29 21:13:50 +0100

    Hacked in CFLAGS

--- wasm_build.sh
+++ wasm_build.sh
@@ -12,16 +12,17 @@ WASM_TRIPLE=wasm32-unknown-wasm
 WASM_BUILD=wasm32
 WASM_SYSROOT=${EMCC_ROOT}/sysroot
 
-export CC=${EMCC_BIN}/wasm32-clang
+export CFLAGS="--sysroot=${WASM_SYSROOT}"
+export CPPFLAGS="${CFLAGS} -DHAVE_UNSETENV=1 -DHAVE_PUTENV=1 -DHAVE_TIMEGM=1 -DHAVE_FORK=1"
+
+export CC="${EMCC_BIN}/wasm32-clang ${CFLAGS}"
 export CXX=${EMCC_BIN}/wasm32-clang++
 export AR=${EMCC_BIN}/llvm-ar
 export RANLIB=${EMCC_BIN}/llvm-ranlib
 export SYSROOT=${EMCC_ROOT}/sysroot
 export LDSHARED=${EMCC_BIN}/wasm-ld
-export CFLAGS="--sysroot=${WASM_SYSROOT}"
-export CPPFLAGS="${CFLAGS} -DHAVE_UNSETENV=1 -DHAVE_PUTENV=1 -DHAVE_TIMEGM=1 -DHAVE_FORK=1"
 
-./configure FLAGS_GENERAL=${CFLAGS} --enable-lto=no --enable-cuda=no --host=${WASM_TRIPLE} --build=${WASM_BUILD}
+./configure --enable-lto=no --enable-cuda=no --host=${WASM_TRIPLE} --build=${WASM_BUILD}
 
 make
 
