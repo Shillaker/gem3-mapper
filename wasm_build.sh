c357eab297e62df7399c34375db16dfedfbd4789
Simon Shillaker <shillaker@gmail.com>
2019-07-15 18:52:57 +0100

    Fixed compilation and added upload script

--- wasm_build.sh
+++ wasm_build.sh
@@ -12,7 +12,15 @@ WASM_TRIPLE=wasm32-unknown-wasm
 WASM_BUILD_TYPE=wasm32
 WASM_SYSROOT=${EMCC_ROOT}/sysroot
 
-export CFLAGS="--sysroot=${WASM_SYSROOT}"
+export CFLAGS="--sysroot=${WASM_SYSROOT}  \
+    -Xlinker --no-entry \
+    -Xlinker --export=main \
+    -Xlinker --max-memory=1073741824 \
+    -Xlinker --stack-first \
+    -Xlinker --threads \
+    -Wl,-z,stack-size=4194304 -Wl, \
+"
+
 export CPPFLAGS="${CFLAGS} -DHAVE_UNSETENV=1 -DHAVE_PUTENV=1 -DHAVE_TIMEGM=1 -DHAVE_FORK=1"
 
 export CC="${EMCC_BIN}/wasm32-clang ${CFLAGS}"
---
+++ wasm_upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+THIS_DIR=$(dirname $(readlink -f $0))
+FILE_PATH=${THIS_DIR}/bin/gem-mapper
+
+FAASM_HOST=localhost
+URL=http://${FAASM_HOST}:8002/f/gene/mapper
+
+# Do the actual upload
+echo "Uploading ${FILE_PATH} to ${URL}"
+curl -X PUT ${URL} -T ${FILE_PATH}
